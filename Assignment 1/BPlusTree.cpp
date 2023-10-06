dd5a59c05d0b78394b3250f4ea9e8a11a2a791aa
Daniel <dgtutv@gmail.com>
2023-10-06 16:14:56 -0700

    Started implementation of program

--- Assignment 1/BPlusTree.cpp
+++ Assignment 1/BPlusTree.cpp
@@ -1,3 +1,13 @@
 #include "BPlusTree.h"
 #include <algorithm>
 using namespace std;
+
+/*------------------------------------------Node----------------------------------------*/
+Node::Node(Node* parent, bool isLeaf) : parent(parent), isLeaf(isLeaf){};
+
+/*----------------------------------------BPlusTree-------------------------------------*/
+BPlusTree::BPlusTree(int maxNumPointers) : maxNumPointers(maxNumPointers){};
+
+bool BPlusTree::insert(int key, string value){
+    
+}
