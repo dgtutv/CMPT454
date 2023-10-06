03d75e1682eb7288e6bcbfaca2198a102422a1dd
Daniel <dgtutv@gmail.com>
2023-10-06 12:25:30 -0700

    Working on implementing new versions of node and BPlusTree

--- Assignment 1/BPlusTree.cpp
+++ Assignment 1/BPlusTree.cpp
@@ -7,7 +7,11 @@ bool compareNodes(const Node* a, const Node* b){     //This function is used to 
 }
 
 /*---------------------------------------------------Node--------------------------------------------------------*/
-Node::Node(int key, string value) : key(key), value(value){};   //Constructor
+Node::Node(int key, string value, int maxNumPointers) : key(key), value(value), maxNumPointers(maxNumPointers){     //Constructor
+    this->isLeaf = false;
+    this->isRoot = false;
+    this->isInterior = false;
+};   
 
 string Node::getValue() const{      
     return this->value;
@@ -17,118 +21,36 @@ int Node::getKey() const{
     return this->key;
 }
 
-/*-------------------------------------------------Leaf Node-----------------------------------------------------*/
-leafNode::leafNode(int key, string value, Node* parent) : Node(key, value), parent(parent){};       //Constructor
-
-Node* leafNode::getParent() const{
-    return this->parent;
-}
-
-list<leafNode*> leafNode::getSiblings() const{
-    return this->siblings;
-}
-
-leafNode* leafNode::addSibling(leafNode* sibling){      //Returns a pointer to the sibling upon succession, returns NULL upon failure
-    if(find(this->siblings.begin(), this->siblings.end(), sibling) == this->siblings.end()){   //Check if the node is already one of the siblings
-        this->siblings.push_back(sibling);  //If not, add it to the list
-        this->siblings.sort(compareNodes);  //Sort the list by node's keys
-        return sibling;
-    }
-    return NULL;
-}
-
-Node* leafNode::changeParent(Node* newParent){
-    Node* oldParent = this->parent;
-    this->parent = newParent;
-    return oldParent;
-}
-
-Node* leafNode::removeSibling(leafNode* sibling){
-    if(find(this->siblings.begin(), this->siblings.end(), sibling) != this->siblings.end()){    //If the node is in the siblings list
-        this->siblings.remove(sibling);      //Remove it
-        return sibling;     //Return a pointer to it
-    }
-    return NULL;
-}
-
-/*-----------------------------------------------Interior Node---------------------------------------------------*/
-interiorNode::interiorNode(int key, string value, Node* parent) : Node(key, value), parent(parent){};       //Constructor
-
-list<interiorNode*> interiorNode::getSiblings() const{
-    return this->siblings;
-}
-
-list<Node*> interiorNode::getChildren() const{
+list<Node*> Node::getChildren() const{
     return this->children;
 }
 
-Node* interiorNode::getParent() const{
+Node* Node::getParent() const{
     return this->parent;
 }
 
-interiorNode* interiorNode::addSibling(interiorNode* sibling){      //Returns a pointer to the sibling upon succession, returns NULL upon failure
-    if(find(this->siblings.begin(), this->siblings.end(), sibling) == this->siblings.end()){   //Check if the node is already one of the siblings
-        this->siblings.push_back(sibling);  //If not, add it to the list
-        this->siblings.sort(compareNodes);  //Sort the list by node's keys
-        return sibling;
-    }
-    return NULL;
-}
-
-Node* interiorNode::changeParent(Node* newParent){
-    Node* oldParent = this->parent;
+Node* Node::setParent(Node* newParent){
+    Node* returnParent = this->parent;
     this->parent = newParent;
-    return oldParent;
+    return(returnParent);
 }
 
-Node* interiorNode::removeSibling(interiorNode* sibling){
-    if(find(this->siblings.begin(), this->siblings.end(), sibling) != this->siblings.end()){    //If the node is in the siblings list
-        this->siblings.remove(sibling);      //Remove it
-        return sibling;     //Return a pointer to it
+bool Node::addChild(Node* newChild){
+    if(this->children.size() < this->maxNumPointers){   //First check if there is room for the child
+        //Then check if a node with the same value is already present
+        for(auto iterator = this->children.begin(); iterator !=this->children.end(); ++iterator){
+            Node* currentChild = *iterator;
+            if(currentChild->getKey() == newChild->getKey()){
+                return false;
+            }
+        }
+        //If not, add it and sort the children list
+        this->children.push_back(newChild);
+        this->children.sort(compareNodes);
+        return true;
     }
-    return NULL;
+    return false;
 }
-Node* interiorNode::removeChild(Node* child){
-    if(find(this->children.begin(), this->children.end(), child) != this->children.end()){      //If the node is in the children list
-        this->children.remove(child);
-        return child;
-    }
-    return NULL;
-}
-
-Node* interiorNode::addChild(Node* child){      //Returns a pointer to the child upon succession, returns NULL upon failure 
-    if(find(this->children.begin(), this->children.end(), child) == this->children.end()){  //If the node is not in the children list
-        this->children.push_back(child);    //Add the child
-        this->children.sort(compareNodes);  //Sort the list
-        return child;
-    }
-    return NULL;
-}
-
-/*---------------------------------------------------Root Node---------------------------------------------------------*/
-rootNode::rootNode(int key, string value):Node(key, value){};       //Constructor
-
-list<Node*> rootNode::getChildren() const{
-    return this->children;
-}
-
-Node* rootNode::addChild(Node* child){      //Returns a pointer to the child upon succession, returns NULL upon failure
-    if(find(this->children.begin(), this->children.end(), child) == this->children.end()){  //If the node is not in the children list
-        this->children.push_back(child);    //Add the child
-        this->children.sort(compareNodes);  //Sort the list
-        return child;
-    }
-    return NULL;
-}
-
-Node* rootNode::removeChild(Node* child){
-    if(find(this->children.begin(), this->children.end(), child) != this->children.end()){      //If the node is in the children list
-        this->children.remove(child);
-        return child;
-    }
-    return NULL;
-}
-
 /*--------------------------------------------------B+ Tree-----------------------------------------------------------*/
 BPlusTree::BPlusTree(int maxNumPointers):maxNumPointers(maxNumPointers){};      //Constructor
 
--- Assignment 1/BPlusTree.h
+++ Assignment 1/BPlusTree.h
@@ -8,13 +8,14 @@ class Node {
         int key;
         Node* parent;
         list<Node*> children;
+        int maxNumPointers;
     public:
         bool isLeaf;
         bool isInterior;
         bool isRoot;
 
         //Constructor
-        Node(int key, string value);
+        Node(int key, string value, int maxNumPointers);
 
         //Getters
         string getValue() const;
@@ -22,10 +23,10 @@ class Node {
         list<Node*> getChildren() const;
         Node* getParent() const;
 
-        //Setters, return true on success, false on failure
-        bool setParent(Node* newParent);
-        bool addChild(Node* newChild);
-        bool removeChild(int value);
+        //Setters
+        Node* setParent(Node* newParent);   //Returns a pointer to the old parent
+        bool addChild(Node* newChild);      //return true on success, false on failure
+        Node* removeChild(int value);       //Returns a pointer to the removed child
 };
 
 class BPlusTree {
