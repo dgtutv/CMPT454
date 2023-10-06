4269e4f8785c0bd2c9b25ff50a443c39741bc704
Daniel <dgtutv@gmail.com>
2023-10-06 15:07:06 -0700

    Starting new version of header file

--- Assignment 1/BPlusTree.h
+++ Assignment 1/BPlusTree.h
@@ -7,7 +7,31 @@ class Pair{
         int key;
         string value;
     public:
+        //Constructor
         Pair(int key, string value);
+
+        //Getter methods
         string getValue() const;
         int getKey() const;
+};
+
+class leafNode{
+    private:
+        list<Pair> pairs;
+        void* parent;
+        leafNode* leftSibling;
+        leafNode* rightSibling;
+    public:
+        //Constructor
+        leafNode(void* parent, leafNode* leftSibling, leafNode* rightSibling);
+
+        //Getter methods
+        void* getParent() const;
+        leafNode* getLeftSibling() const;
+        leafNode* getRightSibling() const;
+
+        //Setter methods
+        void* changeParent(void* newParent);    //Returns a pointer to the old parent
+        leafNode* changeLeftSibling(leafNode* newLeftSibling);      //Returns a pointer to the old left sibling
+        leafNode* changeRightSibling(leafNode* newRightSibling);    //Returns a pointer to the old right sibling
 };
