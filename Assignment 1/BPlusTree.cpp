0a22b026d8a75b882daed0f5355d8d6cb2608912
Daniel <dgtutv@gmail.com>
2023-10-11 09:00:37 -0700

    Working on insert method

--- Assignment 1/BPlusTree.cpp
+++ Assignment 1/BPlusTree.cpp
@@ -1,5 +1,6 @@
 #include "BPlusTree.h"
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
 /*------------------------------------------Node----------------------------------------*/
@@ -8,6 +9,13 @@ Node::Node(Node* parent, bool isLeaf) : parent(parent), isLeaf(isLeaf){};
 /*----------------------------------------BPlusTree-------------------------------------*/
 BPlusTree::BPlusTree(int maxNumPointers) : maxNumPointers(maxNumPointers){};
 
-bool BPlusTree::insert(int key, string value){
-    
-}
+//Each interior/root node must contain floor(maxNumPointers/2) pointers. => floor(maxNumPointers-1/2) values
+bool BPlusTree::insert(int key, string value, Node* localRoot){
+    if(root == NULL){   //If the root does not exist, enter the value into it
+        root = new Node(NULL, true);
+        root->keyValues.insert(pair<int, string>(key, value));
+    }
+    else if(root->keyValues.size() < floor((maxNumPointers-1)/2)){      //If the root has less than half full, insert into the root 
+        root->keyValues.insert(pair<int, string>(key, value));
+    }
+}     
