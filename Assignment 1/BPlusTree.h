5317d67f730057a8e6a4a36bd8c334d25856605a
Daniel <dgtutv@gmail.com>
2023-10-14 03:03:07 -0700

    Completely rewriting code

--- Assignment 1/BPlusTree.h
+++ Assignment 1/BPlusTree.h
@@ -1,66 +1,39 @@
-#include <vector>
 #include <string>
-#include <map>
 using namespace std;
 
-class BPlusTree {
-    public:
-        class Node{
-            private:
-                Node* parent;
-                bool isLeaf;
-                BPlusTree* tree;
-                Node* nextLeaf;     //Is null if the node is not a leaf itself
-            public:
-                //Public data
-                map<int, string> keyValues;     //Here I use a map to store key/value pairs
-                vector<Node*> children;       //Will be empty if the Node is a leafNode
-
-                //Constructor
-                Node(Node* parent, bool isLeaf, BPlusTree* tree);    
-                
-                //Helpers
-                bool isOverflow() const;        //Checks if the Node is past full
-                bool isFull() const;        //Checks if the Node is full
-                friend class BPlusTree;
-
-                //Destructor
-                ~Node();
-        };
+class Node{
     private:
-
+        int* keys;
+        Node** children;
+        string* values;
+        Node* parent;
+        int size;
+        bool isLeaf;
     public:
+        Node(int n); 
+};
+
+class BPlusTree{
+    private:
+        int maxNumKeys;
         Node* root;
-        vector<Node*> allNodes;       //Used for memory purposes
-        int maxNumPointers; 
+    public:
+        BPlusTree(int n);     //Constructor
+        BPlusTree(BPlusTree& treeToCopy);       //Copy constructor
+        BPlusTree& operator=(BPlusTree& treeToCopy);       //Overloaded equals operator
+        ~BPlusTree();       //Destructor
 
-        //Constructor
-        BPlusTree(int maxNumPointers);  //Maximum number of entries is maxNumPointers-1
-
-        //Setters     
+        //Required methods
         bool insert(int key, string value);
         bool remove(int key);
-
-        //Getters
         string find(int key);
         void printKeys();
         void printValues();
-        void printLeaves();     //Goes to the first leaf, then uses leaf pointers to get the rest
 
-        //Helpers
-        void printNodeKey(Node* node);  //Prints the keys of the specified node
-        void printNodeValue(Node* node);  //Prints the value of the specified node
-        void splitNode(Node* leftNode, int key, string value);   //Splits a node to right and left nodes, and inserts a new value
-        Node* findNode(int key);        //Finds the leaf node that should contain a given key
-        void redistribute(Node* victim, Node* receiver, bool victimLeftOfReceiver);    //Redistributes a value (and a pointer if interior) from the victim to the reciever
-        void coalesce(Node* victim, Node* receiver, bool victimLeftOfReceiver);        //Coalesces victim into receiver
-        void removeFromNode(Node* node, int key, Node* pointer);        //Pointer is null if removing from a leaf
-        int findIndexOfNodeInParent(Node* child);
-        int findAssociatedKeyOfNodeInParent(Node* child);
-        Node* findLeafToLeftOfNode(Node* node);
-        void updateParentPointers(Node* parent);        //Iterates over the entire tree and updates the parent pointers for all nodes, pass in the root
-        void fixPointers();         //Ensures that the tree is properly linked in terms of children and parents
-
-        //Destructor
-        ~BPlusTree();
-};
+        //Helper methods
+        Node* copyNode(Node* nodeToCopy);
+        bool insertInLeaf(Node* leftNode, int key, string value);
+        bool insertInterior(Node* leftNode, int key, Node* child);
+        bool redistribute(Node* victim, Node* receiver);
+        bool coalesce(Node* victim, Node* receiver);
+}
